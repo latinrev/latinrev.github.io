1db034e30e753ca96b6cdebb34bdd7cc016d37f8
Revlink <latinreve@gmail.com>
2020-01-18 08:51:45 -0430

    First release new dev portfolio

--- resources/js/app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    //STICKY NAV
-    
-$('.js-projects').click(function(){
-    $('html, body').animate({scrollTop: $('.projects-section').offset().top},1000);
-  });
-});
